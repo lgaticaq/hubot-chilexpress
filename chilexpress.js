462708551ee197a75db571b1c8efeacf15d4f5a9
Leonardo Gatica <lgatica@protonmail.com>
2019-11-21 10:33:11 -0300

    fix: replace scrapping to chilexpress lib

--- chilexpress.js
+++ chilexpress.js
@@ -2,8 +2,7 @@
 //   Obtiene info de despacho de un producto a través de Chilexpress
 //
 //  Dependencies:
-//    cheerio
-//    string
+//    chilexpress
 //
 // Commands:
 //   hubot chilexpress <CODIGO SEGUIMIENTO>
@@ -11,35 +10,27 @@
 // Author:
 //   @juanbrujo
 
-var cheerio = require('cheerio');
-var S       = require('string');
+var chilexpress = require('chilexpress');
 
 module.exports = function(robot) {
   robot.respond(/chilexpress\s?(.*)/i, function(msg) {
 
     var codigo = msg.match[1];
-    var url = 'https://www.chilexpress.cl/Views/ChilexpressCL/Resultado-busqueda.aspx?DATA=' + codigo;
-
     msg.send(':mailbox_closed: buscando...');
-
-    msg.robot.http(url).get()(function(err, res, body) {
-
-      var $ = cheerio.load(body);
-
-      if (res.statusCode !== 200 || body === 'ERROR') {
-        return msg.reply('Ocurrió un error con la búsqueda');
-      }
-
-      var title = $('.search-results .wigdet-content ul > li > a > h4').text();
-      var estado = $('.search-results .wigdet-content ul > li ul li').eq(3).text();
-
+    chilexpress(codigo).then(function(data) {
+  
+      var title = data.shipping.product;
+      if (data.history.length > 0) title = data.history[0].activity
       if( $('.search-results .wigdet-content').length) { // existe
-        msg.send('¡Encontré! ' + title + ': ' + estado);
+        msg.send('¡Encontré! ' + title + ': ' + data.status);
       } else {
         msg.send('Orden no existe.');
       }
-      
+  
+    }).catch(function(err) {
+      robot.emit('error', err);
+      return msg.reply('Ocurrió un error con la búsqueda');
     });
-
+  
   });
 };
